0ffb5cc6d73990de447d9abf8cb89e76f876b21c
jkugalde <jcugaldecasmail@gmail.com>
2019-10-19 01:46:35 -0300

    Tested on protoboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,41 @@
 #include <Arduino.h>
 #include <QTRSensors.h>
 
-#define _pinM1I 3
-#define _pinM1D 4
-#define _pinM1PWM 5
+#define _pinM1I 4
+#define _pinM1D 5
+#define _pinM1PWM 3
 
-#define _pinM2I 6
-#define _pinM2D 7
-#define _pinM2PWM 8
+#define _pinM2I 7
+#define _pinM2D 8
+#define _pinM2PWM 6
 
 #define _pinBuzz 9
-#define _stdby 10
+#define _stdby 2
 #define _pinbutton 11
+#define _pinemitter 10
+
+#define _calibsound 440
+#define _curvsound 1000
 
 int _maxspeed = 155;
 int _minspeed = 70;
 
-#define srate 100
 #define ethreshold 200
 
+unsigned long srate = 100;
+
 int _sM1I = 0;
-int _sM1D = 0;
-int _sM2I = 0;
+int _sM1D = 1;
+int _sM2I = 1;
 int _sM2D = 0;
 
-float Kp = 1;
-float Kd = 1;
-float Ki = 1;
+float Kp = 0.01;
+float Kd = 0.1;
+float Ki = 0.01;
 
 int position = 0;
 int error = 0;
+float lasterror = 0;
 float derror = 0;
 int ierror = 0;
 int ithreshold = 100;
@@ -43,10 +49,10 @@ unsigned long debouncetime = 0;
 unsigned long lastdebounceleft = 0;
 unsigned long lastdebounceright = 0;
 
-int rightmarkervalue = 0;
-int lastrightmarkervalue = 0;
-int leftmarkervalue = 0;
-int lastleftmarkervalue = 0;
+uint16_t rightmarkervalue = 0;
+uint16_t lastrightmarkervalue = 0;
+uint16_t leftmarkervalue = 0;
+uint16_t lastleftmarkervalue = 0;
 
 boolean _incurvemarker = false;
 boolean _incurve = false;
@@ -69,7 +75,7 @@ uint16_t rightsensorValues[rightsensors];
 void pidout()
 {
   error = 2500 - position;
-  derror = (derror - error) / srate;
+  derror = float((lasterror - error)) / srate;
   if (abs(error) < ithreshold)
   {
     ierror = ierror + error;
@@ -79,7 +85,7 @@ void pidout()
     ierror = 0;
   }
   sign = Kp * error + Ki * ierror + Kd * derror;
-
+  lasterror=error;
   leftspeed = leftspeed - sign;
   rightspeed = rightspeed + sign;
 
@@ -94,16 +100,20 @@ void pidout()
 
   leftspeed = constrain(leftspeed, _minspeed, _maxspeed);
   rightspeed = constrain(rightspeed, _minspeed, _maxspeed);
+  Serial.print(leftspeed);
+  Serial.print("     ");
+  Serial.println(rightspeed);
+
 }
 
 void move()
 {
   digitalWrite(_pinM1D, _sM1D);
   digitalWrite(_pinM1I, _sM1I);
-  digitalWrite(_pinM1PWM, leftspeed);
+  analogWrite(_pinM1PWM, leftspeed);
   digitalWrite(_pinM2D, _sM2D);
   digitalWrite(_pinM2I, _sM2I);
-  digitalWrite(_pinM1PWM, rightspeed);
+  analogWrite(_pinM2PWM, rightspeed);
 }
 
 void end()
@@ -119,11 +129,12 @@ void end()
   }
   if (rmarkercount == 2)
   {
+
     digitalWrite(_stdby, LOW);
   }
 }
 
-void debounce(int value, int lastvalue, unsigned long lastdebounce, boolean state)
+boolean debounce(int value, int lastvalue, unsigned long lastdebounce, boolean state)
 {
 
   if (value != lastvalue)
@@ -139,11 +150,12 @@ void debounce(int value, int lastvalue, unsigned long lastdebounce, boolean stat
     }
   }
   lastvalue = value;
+  return state;
 }
 
 int makedigital(int value)
 {
-  if (value > 750)
+  if (value < 500)
   {
     return 1;
   }
@@ -171,31 +183,72 @@ void sensefront()
   position = _front.readLineWhite(frontsensorValues);
 }
 
-void sensesides(){
-  leftmarkervalue = makedigital(_left.readLineWhite(leftsensorValues));
-  rightmarkervalue = makedigital(_right.readLineWhite(rightsensorValues));
-  debounce(leftmarkervalue, lastleftmarkervalue, lastdebounceleft, _incurvemarker);
-  debounce(rightmarkervalue, lastrightmarkervalue, lastdebounceright, _inracemarker); 
+void sensesides()
+{
+
+  _right.readLineBlack(rightsensorValues);
+  rightmarkervalue = makedigital(rightsensorValues[0]);
+
+  if (rightmarkervalue != lastrightmarkervalue)
+  {
+    lastdebounceright = millis();
+  }
+  if ((millis() - lastdebounceright) > debouncetime)
+  {
+
+    if (rightmarkervalue != _inracemarker)
+    {
+      _inracemarker = rightmarkervalue;
+    }
+  }
+  lastrightmarkervalue = rightmarkervalue;
+
+  _left.readLineBlack(leftsensorValues);
+  leftmarkervalue = makedigital(leftsensorValues[0]);
+
+  if (leftmarkervalue != lastleftmarkervalue)
+  {
+    lastdebounceleft = millis();
+  }
+  if ((millis() - lastdebounceleft) > debouncetime)
+  {
+
+    if (leftmarkervalue != _incurvemarker)
+    {
+      _incurvemarker = leftmarkervalue;
+    }
+  }
+  lastleftmarkervalue = leftmarkervalue;
 }
 
 void setupSensors()
 {
 
-  _front.setTypeAnalog();
-  _front.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5}, frontsensors);
-  _front.setEmitterPin(2);
-  _left.setTypeAnalog();
-  _left.setSensorPins((const uint8_t[]){A6}, leftsensors);
-  _left.setEmitterPin(2);
+  tone(_pinBuzz, _calibsound);
+  delay(1000);
+  noTone(_pinBuzz);
+
   _right.setTypeAnalog();
   _right.setSensorPins((const uint8_t[]){A7}, rightsensors);
-  _right.setEmitterPin(2);
+  _left.setTypeAnalog();
+  _left.setSensorPins((const uint8_t[]){A6}, leftsensors);
+  _front.setTypeAnalog();
+  _front.setSensorPins((const uint8_t[]) {A0,A1,A2,A3,A4,A5}, frontsensors);
+
   for (int i = 0; i < 200; i++)
   {
     _front.calibrate();
     _right.calibrate();
     _left.calibrate();
   }
+
+  tone(_pinBuzz, _calibsound);
+  delay(500);
+  noTone(_pinBuzz);
+  delay(500);
+  tone(_pinBuzz, _calibsound);
+  delay(500);
+  noTone(_pinBuzz);
 }
 
 void setupWheels()
@@ -209,10 +262,10 @@ void setupWheels()
   pinMode(_pinBuzz, OUTPUT);
   pinMode(_stdby, OUTPUT);
   digitalWrite(_stdby, HIGH);
-  digitalWrite(_sM1D, HIGH);
-  digitalWrite(_sM1I, LOW);
-  digitalWrite(_sM2D, HIGH);
-  digitalWrite(_sM2I, LOW);
+  digitalWrite(_pinM1D,_sM1D);
+  digitalWrite(_pinM1I,_sM1I);
+  digitalWrite(_pinM2D,_sM2D);
+  digitalWrite(_pinM2I, _sM2I);
 }
 
 void waitbutton()
@@ -220,27 +273,30 @@ void waitbutton()
   while (digitalRead(_pinbutton) == false)
   {
   }
+  tone(_pinBuzz, 2000);
   delay(3000);
 }
 
 void setup()
 {
+  Serial.begin(9600);
   setupWheels();
   setupSensors();
   pinMode(_pinBuzz, OUTPUT);
   pinMode(_pinbutton, OUTPUT);
-  waitbutton();
+  //waitbutton();
 }
 
 void loop()
 {
   if (millis() - tactual >= srate)
   {
-    sensefront();
-    pidout();
+    sensesides();
     turning();
     end();
+    sensefront();
+    pidout();
     move();
+    tactual = millis();
   }
-  sensesides();
 }
